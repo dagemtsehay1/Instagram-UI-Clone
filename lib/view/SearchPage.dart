6e3962a50cdf743eebc8cd8d93130d6c4b848261
dagem tsehay <dagemtsehay4@gmail.com>
2021-10-22 19:43:30 +0300

    search screen fixed

--- lib/view/SearchPage.dart
+++ lib/view/SearchPage.dart
@@ -1,6 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
+  final List<String> pics = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +39,27 @@ class SearchPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          "Search Screen",
-          style: TextStyle(
-            fontSize: 40,
-          ),
+      body: GridView.builder(
+        physics: BouncingScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
         ),
+        itemCount: pics.length,
+        itemBuilder: (contex, index) {
+          return Padding(
+            padding: EdgeInsets.all(1),
+            child: Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/${pics[index]}"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
