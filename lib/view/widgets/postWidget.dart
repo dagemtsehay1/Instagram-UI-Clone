341657eb67f8dc8fbd325b5edc0dc00fe53d8473
dagem tsehay <dagemtsehay4@gmail.com>
2021-10-22 16:00:23 +0300

    post widget fixed

--- lib/view/widgets/postWidget.dart
+++ lib/view/widgets/postWidget.dart
@@ -10,8 +10,8 @@ class PostWidget extends StatefulWidget {
 }
 
 class _PostWidgetState extends State<PostWidget> {
-  bool isLiked = false;
   bool isBookmarked = false;
+  bool isLiked = false;
   List<Post> posts = [
     Post(
       author: "yawen_heng",
@@ -61,194 +61,198 @@ class _PostWidgetState extends State<PostWidget> {
     Post(
       author: "bob_arnold",
       profilePic: "9.jpg",
-      postPic: "4.jpg",
+      postPic: "0.jpg",
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: Card(
-        color: Colors.white,
-        elevation: 1.0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
+    return Column(
+      children: posts.map((e) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Card(
+            color: Colors.white,
+            elevation: 1.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Container(
-                        height: 60,
-                        width: 60,
-                        margin: EdgeInsets.all(3.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          image: DecorationImage(
-                            image: AssetImage("images/1.jpg"),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Row(
                         children: <Widget>[
-                          Text(
-                            "rita_higgins",
-                            style: TextStyle(
-                              letterSpacing: 1,
-                              fontWeight: FontWeight.w700,
+                          Container(
+                            height: 60,
+                            width: 60,
+                            margin: EdgeInsets.all(3.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              image: DecorationImage(
+                                image: AssetImage("images/${e.profilePic}"),
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                           SizedBox(
-                            height: 5,
+                            width: 10,
                           ),
-                          Text(
-                            "Addis Abeba, Ethiopia",
-                            style: TextStyle(
-                              letterSpacing: 1,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.grey,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "${e.author}",
+                                style: TextStyle(
+                                  letterSpacing: 1,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Addis Abeba, Ethiopia",
+                                style: TextStyle(
+                                  letterSpacing: 1,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.more_horiz,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                Image(
+                  image: AssetImage("images/${e.postPic}"),
+                  width: double.infinity,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          IconButton(
+                            icon: isLiked
+                                ? Icon(
+                                    Icons.favorite,
+                                    color: Colors.pink,
+                                    size: 30,
+                                  )
+                                : Icon(
+                                    Icons.favorite_border,
+                                    color: Colors.black,
+                                    size: 30,
+                                  ),
+                            onPressed: () {
+                              setState(() {
+                                isLiked = !isLiked;
+                              });
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.add_comment,
+                              size: 30,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                isLiked = !isLiked;
+                              });
+                            },
+                          ),
+                          Transform.rotate(
+                            angle: -math.pi / 4,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.send,
+                                color: Colors.black,
+                                size: 30,
+                              ),
+                              onPressed: () {},
                             ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.more_horiz,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
-            Image(
-              image: AssetImage("images/9.jpg"),
-              width: double.infinity,
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
                       IconButton(
-                        icon: isLiked
+                        icon: isBookmarked
                             ? Icon(
-                                Icons.favorite,
-                                color: Colors.pink,
+                                Icons.bookmark,
                                 size: 30,
+                                color: Colors.pink,
                               )
                             : Icon(
-                                Icons.favorite_border,
-                                color: Colors.black,
+                                Icons.bookmark_border,
                                 size: 30,
+                                color: Colors.black,
                               ),
                         onPressed: () {
                           setState(() {
-                            isLiked = !isLiked;
+                            isBookmarked = !isBookmarked;
                           });
                         },
                       ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.add_comment,
-                          size: 30,
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: 20,
+                    top: 0,
+                    right: 20,
+                    bottom: 10,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Liked by ",
+                        style: TextStyle(
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.w600,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            isLiked = !isLiked;
-                          });
-                        },
                       ),
-                      Transform.rotate(
-                        angle: -math.pi / 4,
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.send,
-                            color: Colors.black,
-                            size: 30,
-                          ),
-                          onPressed: () {},
+                      Text(
+                        "dagem_tsehay",
+                        style: TextStyle(
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        " and",
+                        style: TextStyle(
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        " 518 Others",
+                        style: TextStyle(
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ],
                   ),
-                  IconButton(
-                    icon: isBookmarked
-                        ? Icon(
-                            Icons.bookmark,
-                            size: 30,
-                            color: Colors.pink,
-                          )
-                        : Icon(
-                            Icons.bookmark_border,
-                            size: 30,
-                            color: Colors.black,
-                          ),
-                    onPressed: () {
-                      setState(() {
-                        isBookmarked = !isBookmarked;
-                      });
-                    },
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 20,
-                top: 0,
-                right: 20,
-                bottom: 10,
-              ),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "Liked by ",
-                    style: TextStyle(
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    "dagem_tsehay",
-                    style: TextStyle(
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    " and",
-                    style: TextStyle(
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    " 518 Others",
-                    style: TextStyle(
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
