7154c46f53141c25208c57f0037c8f692430a4ad
dagem tsehay <dagemtsehay4@gmail.com>
2021-10-22 20:07:08 +0300

    adding appbar to profile screen

--- lib/view/ProfilePage.dart
+++ lib/view/ProfilePage.dart
@@ -4,6 +4,16 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "dagem_tsehay",
+          style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Text(
           "Profile",
