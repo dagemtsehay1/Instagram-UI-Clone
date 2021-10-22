88c1453e4a69baa64b87229e3ff80d9d5a5fe27c
dagem tsehay <dagemtsehay4@gmail.com>
2021-10-22 22:03:35 +0300

    adding appbar to notification screen

--- lib/view/NotficationPage.dart
+++ lib/view/NotficationPage.dart
@@ -1,9 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              "Following",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 25,
+                letterSpacing: 1,
+              ),
+            ),
+            Text(
+              "You",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                letterSpacing: 1,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text(
           "Notification",
