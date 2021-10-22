74d956d485501e176d4c6095f2037ef240327d6a
dagem tsehay <dagemtsehay4@gmail.com>
2021-10-22 19:05:41 +0300

    adding appbar to Search screen

--- lib/view/SearchPage.dart
+++ lib/view/SearchPage.dart
@@ -4,6 +4,18 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.search,
+          color: Colors.black,
+        ),
+        title: Text(
+          "Search",
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+      ),
       body: Center(
         child: Text(
           "Search Screen",
