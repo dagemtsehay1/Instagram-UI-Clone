bb6f098b85d5cb849fda25d394114e0c05d00099
dagem tsehay <dagemtsehay4@gmail.com>
2021-10-23 13:42:38 +0300

    cleaning the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/view/AddPostPage.dart';
-import 'package:instagram_clone/view/HomePage.dart';
-import 'package:instagram_clone/view/NotficationPage.dart';
-import 'package:instagram_clone/view/ProfilePage.dart';
-import 'package:instagram_clone/view/SearchPage.dart';
+
 import 'package:splashscreen/splashscreen.dart';
+import 'package:instagram_clone/view/MainScreen.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -41,56 +38,5 @@ class _MyAppState extends State<MyApp> {
         styleTextUnderTheLoader: new TextStyle(),
         photoSize: 50,
         loaderColor: Colors.white);
-  }
-}
-
-// main screen
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int currentScreen = 0;
-  List<IconData> iconsList = [
-    Icons.home,
-    Icons.search,
-    Icons.add_box,
-    Icons.favorite_border,
-    Icons.person
-  ];
-
-  var screens = [
-    HomePage(),
-    SearchPage(),
-    AddNewPost(),
-    NotificationPage(),
-    ProfilePage(),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[currentScreen],
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: iconsList.map((icon) {
-            int index = iconsList.indexOf(icon);
-            return IconButton(
-              icon: Icon(
-                icon,
-                size: currentScreen == index ? 30 : 25,
-                color: currentScreen == index ? Colors.black : Colors.grey,
-              ),
-              onPressed: () {
-                setState(() {
-                  currentScreen = index;
-                });
-              },
-            );
-          }).toList(),
-        ),
-      ),
-    );
   }
 }
